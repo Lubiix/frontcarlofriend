2317714474c188d78c899d633f267b329a290799
Axel Piras <pirasaxel@gmail.com>
2021-08-30 12:20:18 +0200

    after merge

--- App.js
+++ App.js
@@ -3,30 +3,17 @@ import React from "react";
 
 import { NativeBaseProvider, Box } from "native-base";
 
-<<<<<<< HEAD
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from "./screens/HomeScreen";
-import AccountCreationScreenParticulier from "./screens/AccountCreationScreenParticulier";
 import AccountCreationScreenCommercant from "./screens/AccountCreationScreenCommercant";
+import AccountCreationScreenParticulier from "./screens/AccountCreationScreenParticulier";
 import LoginScreen from "./screens/LoginScreen";
-import MenuNav from "./components/MenuNav";
-=======
-
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import HomeScreen from './screens/HomeScreen';
-import AccountCreationScreenCommercant from './screens/AccountCreationScreenCommercant';
-import AccountCreationScreenParticulier from './screens/AccountCreationScreenParticulier'
-import LoginScreen from './screens/LoginScreen';
 import FeedScreen from "./screens/FeedScreen";
-import MenuNav from './components/MenuNav';
-import MapScreen from './screens/MapScreen';
->>>>>>> e24a2b6eab01a063f0b2c22bde46dd39759abe7a
+import MenuNav from "./components/MenuNav";
+import MapScreen from "./screens/MapScreen";
 
 const Stack = createStackNavigator();
 
@@ -45,13 +32,9 @@ export default function App() {
             component={AccountCreationScreenCommercant}
           />
           <Stack.Screen name="Login" component={LoginScreen} />
-<<<<<<< HEAD
           <Stack.Screen name="menu" component={MenuNav} />
-=======
-          <Stack.Screen name="menu" component={MenuNav}/>
-          <Stack.Screen name="feed" component={FeedScreen}/>
-          <Stack.Screen name="map" component={MapScreen}/>
->>>>>>> e24a2b6eab01a063f0b2c22bde46dd39759abe7a
+          <Stack.Screen name="feed" component={FeedScreen} />
+          <Stack.Screen name="map" component={MapScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
