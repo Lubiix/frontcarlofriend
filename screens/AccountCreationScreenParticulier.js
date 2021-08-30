c8a4a0c03924a80d82f9f95d4ba5b831bf766421
Axel Piras <pirasaxel@gmail.com>
2021-08-30 12:49:20 +0200

    création frontend createAccount Particulier

--- screens/AccountCreationScreenParticulier.js
+++ screens/AccountCreationScreenParticulier.js
@@ -1,15 +1,52 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Button } from "native-base";
+import { Button, Input } from "native-base";
 
 const AccountCreationScreenParticulier = (props) => {
   const handleGoCommercant = () => {
     props.navigation.navigate("CreateAccountCommercant");
   };
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: "center" }}>
       <Text>Créer un compte Particulier</Text>
-      <Button onPress={() => handleGoCommercant()}>go commerçant</Button>
+      <Button.Group
+        variant="solid"
+        isAttached
+        space={6}
+        mx={{
+          base: "auto",
+          md: 0,
+        }}
+      >
+        <Button style={{ color: "#62ADEB" }} mr={0}>
+          Particulier
+        </Button>
+        <Button
+          style={{ color: "#62ADEB" }}
+          _text={{
+            color: "white",
+          }}
+          onPress={() => handleGoCommercant()}
+        >
+          Commerçant
+        </Button>
+      </Button.Group>
+      <View style={{ flexDirection: "row", marginTop: 10 }}>
+        <Input w="40%" mx={3} placeholder="Nom" />
+        <Input w="40%" mx={3} placeholder="Prénom" />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 10,
+          justifyContent: "center",
+        }}
+      >
+        <Input w="20%" mx={3} placeholder="Age" />
+        <Input w="20%" mx={3} placeholder="Civilité" />
+      </View>
+      <Input w="80%" mx={3} my={2} placeholder="email" />
+      <Input w="80%" mx={3} my={2} placeholder="password" />
     </View>
   );
 };
