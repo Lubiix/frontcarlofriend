85ec95e53b2047b6c5caa9e9e2a07cd475c98c64
Geoffroy Farrugia <g.farrugia@outlook.com>
2021-08-30 12:53:08 +0200

    Input login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import { View, Text, Button} from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
+import { Input, Center, NativeBaseProvider, Button } from "native-base";
 
 const LoginScreen = (props) => {
-    const handleLogin = () => {
-        props.navigation.navigate("menu")
-    }
-    return (
-        <View style={{flex:1}}>
-            <Text>Login</Text>
-            <Button title="Valider" onPress={()=> handleLogin()}></Button>
-        </View>
-    );
+  const [show, setShow] = React.useState(false);
+
+  const handleClick = () => setShow(!show);
+  const handleLogin = () => {
+    props.navigation.navigate("menu");
+  };
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Login</Text>
+      <Input mx={3} placeholder="Email" />
+      <Input
+        type={show ? "text" : "password"}
+        InputRightElement={
+          <Button
+            ml={1}
+            roundedLeft={0}
+            roundedRight="md"
+            onPress={handleClick}
+          >
+            {show ? "Hide" : "Show"}
+          </Button>
+        }
+        placeholder="Password"
+      />{" "}
+      <Button title="Valider" onPress={() => handleLogin()}></Button>
+    </View>
+  );
 };
 
 export default LoginScreen;
