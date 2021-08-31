052f25f5b47a1e7fac9cc0c91f63e1b5c98e6df3
Geoffroy Farrugia <g.farrugia@outlook.com>
2021-08-31 09:24:50 +0200

    gestion conflit loginFront

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -28,45 +28,6 @@ const LoginScreen = (props) => {
   };
 
   return (
-<<<<<<< HEAD
-    <View style={{ flex: 1, marginTop: 29 }}>
-      <Box
-        bg="primary.400"
-        p={4}
-        _text={{
-          fontSize: "md",
-          fontWeight: "bold",
-          color: "white",
-        }}
-        alignItems="center"
-        mb={3}
-      >
-        Connection
-      </Box>
-      <Input mx={5} my={3} placeholder="Email" />
-      <Input
-        w="90%"
-        mx={5}
-        mt={3}
-        mb={6}
-        type={show ? "text" : "password"}
-        InputRightElement={
-          <Button
-            ml={1}
-            roundedLeft={0}
-            roundedRight="md"
-            onPress={handleClick}
-          >
-            {show ? "Hide" : "Show"}
-          </Button>
-        }
-        placeholder="Password"
-      />
-      <Button mx={5} onPress={() => handleLogin()}>
-        Valider
-      </Button>
-    </View>
-=======
     <Fragment>
       <SafeAreaView style={{ flex: 0, backgroundColor: "#62ADEB" }} />
       <SafeAreaView style={{ flex: 1 }}>
@@ -123,7 +84,6 @@ const LoginScreen = (props) => {
         </Button>
       </SafeAreaView>
     </Fragment>
->>>>>>> loginFront
   );
 };
 
