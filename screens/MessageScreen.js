c477b6a87ec38d61de6bee82716f2e96acbbf56e
Geoffroy Farrugia <g.farrugia@outlook.com>
2021-09-03 12:53:01 +0200

    ajout date front

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -39,10 +39,7 @@ const MessageScreen = (props) => {
             <Avatar color="white" bg={"secondary.700"}>
               GF
             </Avatar>
-            <VStack>
-              <Heading size="sm">Geoffroy</Heading>
-              <Text>Waza</Text>
-            </VStack>
+            <Heading size="sm">Geoffroy</Heading>
           </HStack>
         </HStack>
       </Link>
