6afa9d28aabc56c5b04c3bbb3af96dea282e619c
Lucas Pieroni <pieroni.lucas@gmail.com>
2021-09-08 17:14:46 +0200

    feed to component card

---
+++ components/Card.js
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+
+import { useNavigation } from "@react-navigation/native";
+
+import { Dimensions } from "react-native";
+
+import {
+  Ionicons,
+  MaterialCommunityIcons,
+  AntDesign,
+  FontAwesome5,
+} from "@expo/vector-icons";
+
+import { Box, Avatar, HStack, VStack, Image, Text, Button } from "native-base";
+import { createParser } from "styled-system";
+
+function Card({ item, handleComment, isEvent = false }) {
+  const [countLikePost, setCountLikePost] = useState(0);
+
+  const navigation = useNavigation();
+
+  const handleLike = () => {
+    setCountLikePost(countLikePost + 1);
+  };
+
+  const screenWidth = Dimensions.get("screen").width;
+
+  const date = item.date;
+  const newDate = new Date(date);
+  let dateHours = newDate.getHours();
+  if (dateHours < 10) {
+    dateHours = `0${dateHours}`;
+  }
+  let dateMinutes = newDate.getMinutes();
+  if (dateMinutes < 10) {
+    dateMinutes = `0${dateMinutes}`;
+  }
+  const dateWeek = newDate.toLocaleDateString();
+  const datePost = (
+    <VStack alignItems="center">
+      <Text color="#B6B6B6" style={{ fontSize: 10 }}>
+        {dateWeek} - {dateHours}:{dateMinutes}
+      </Text>
+    </VStack>
+  );
+  return (
+    <VStack>
+      <Box
+        bg="#FFFFFF"
+        p={4}
+        style={{
+          marginTop: 10,
+        }}
+      >
+        <HStack
+          style={{
+            space: 3,
+            marginBottom: 3,
+            justifyContent: "space-between",
+            alignItems: "center",
+            borderBottomColor: "#B6B6B6",
+            borderBottomWidth: 1,
+          }}
+        >
+          <HStack
+            style={{
+              space: 3,
+              marginBottom: 10,
+              alignItems: "center",
+            }}
+          >
+            <Avatar
+              size="lg"
+              source={{
+                uri: "https://pbs.twimg.com/profile_images/1352844693151731713/HKO7cnlW_400x400.jpg",
+              }}
+            ></Avatar>
+            <VStack>
+              <Text
+                style={{ fontWeight: "bold", marginLeft: 5, fontSize: 18 }}
+                color="#000000"
+              >
+                {item.createur.prenom} {item.createur.nom}
+              </Text>
+              <Text style={{ marginLeft: 5 }}>{item.quartier.name}</Text>
+              {datePost}
+            </VStack>
+          </HStack>
+        </HStack>
+        <Text>{item.content}</Text>
+        <Box alignItems="center">
+          <Image
+            source={{
+              uri: item.image
+                ? item.image
+                : "https://www.wallpapersun.com/wp-content/uploads/2021/05/Hasbulla-Wallpaper-13.jpg",
+            }}
+            alt={"Alternate Text"}
+            resizeMode="cover"
+            size="2xl"
+            marginTop={5}
+            marginBottom={5}
+            style={{
+              width: screenWidth,
+              maxHeight: 300,
+            }}
+          />
+        </Box>
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          name="likecommentshare"
+          style={{ padding: 10, marginTop: 0 }}
+        >
+          <Text>
+            <AntDesign
+              name="like2"
+              size={24}
+              color="#B6B6B6"
+              onPress={() => handleLike()}
+            />
+            {countLikePost}
+          </Text>
+          <MaterialCommunityIcons
+            name="comment-multiple-outline"
+            size={24}
+            color="#62ADEB"
+            onPress={() => handleComment(item._id)}
+          />
+          {isEvent && (
+            <FontAwesome5
+              name="location-arrow"
+              size={24}
+              color="#62ADEB"
+              onPress={() =>
+                navigation.navigate("Event", { idEvent: item._id })
+              }
+            />
+          )}
+          <Ionicons name="share-outline" size={28} color="#B6B6B6" />
+        </HStack>
+      </Box>
+    </VStack>
+  );
+}
+
+export default Card;
--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,38 +1,37 @@
 import React, { Fragment, useState, useEffect } from "react";
+import Card from "../components/Card";
 import { View, ScrollView } from "react-native";
 import {
-  Entypo,
   MaterialIcons,
   Ionicons,
   MaterialCommunityIcons,
-  AntDesign,
-  FontAwesome5,
 } from "@expo/vector-icons";
-import {
-  Box,
-  Center,
-  Avatar,
-  NativeBaseProvider,
-  Badge,
-  Flex,
-  Spacer,
-  HStack,
-  VStack,
-  Image,
-  Stack,
-  Modal,
-  Input,
-  Button,
-  Text,
-} from "native-base";
+import { Avatar, HStack, Stack, Modal, Input, Text } from "native-base";
 import { HOST } from "@env";
 import { connect } from "react-redux";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import IconBadge from "react-native-icon-badge";
-import { marginLeft } from "styled-system";
 
 function FeedScreen(props) {
+  // console.log("compteur like actif:", countLikePost);
+
+  const [feedList, setFeedList] = useState([]);
+
+  const [eventList, setEventList] = useState([]);
+  console.log("eventList:", eventList);
+
+  const [commentList, setCommentList] = useState([]);
+  // console.log("commentList", commentList);
+
+  const [commentValue, setCommentValue] = useState("");
+  console.log("commentaire récupéré:", commentValue);
+
+  const [postId, setPostId] = useState("");
+
+  console.log("postId:", postId);
+  // console.log("feedlist:", feedList);
+
+  const [showModal, setShowModal] = useState(false);
   console.log("HOOOOOOST", HOST);
+
   const handleNavigEvent = function () {
     console.log("click");
     setShowModalNotif(false);
@@ -54,30 +53,6 @@ function FeedScreen(props) {
   const closeComment = () => {
     setShowModal(false);
   };
-  const handleLike = () => {
-    setCountLikePost(countLikePost + 1);
-  };
-
-  const [countLikePost, setCountLikePost] = useState(0);
-  // console.log("compteur like actif:", countLikePost);
-
-  const [feedList, setFeedList] = useState([]);
-
-  const [eventList, setEventList] = useState([]);
-  console.log("eventList:", eventList);
-
-  const [commentList, setCommentList] = useState([]);
-  // console.log("commentList", commentList);
-
-  const [commentValue, setCommentValue] = useState("");
-  console.log("commentaire récupéré:", commentValue);
-
-  const [postId, setPostId] = useState("");
-
-  console.log("postId:", postId);
-  // console.log("feedlist:", feedList);
-
-  const [showModal, setShowModal] = useState(false);
 
   //ENVOI COMMENTAIRE AU BACK VIA ROUTE /comment
   const sendComment = async () => {
@@ -172,235 +147,17 @@ function FeedScreen(props) {
     }
   });
   let events = eventList.map((event, index) => {
-    const date = event.date;
-    const newDate = new Date(date);
-    let dateHours = newDate.getHours();
-    if (dateHours < 10) {
-      dateHours = `0${dateHours}`;
-    }
-    let dateMinutes = newDate.getMinutes();
-    if (dateMinutes < 10) {
-      dateMinutes = `0${dateMinutes}`;
-    }
-    const dateWeek = newDate.toLocaleDateString();
-    const dateEvent = (
-      <VStack>
-        <Text>
-          {dateWeek} - {dateHours}:{dateMinutes}
-        </Text>
-      </VStack>
-    );
     return (
-      <VStack key={index}>
-        <Box
-          bg="#FFFFFF"
-          p={4}
-          style={{
-            marginTop: 10,
-            alignSelf: "center",
-          }}
-        >
-          <HStack
-            style={{
-              space: 3,
-              marginBottom: 3,
-              justifyContent: "space-evenly",
-              alignItems: "center",
-            }}
-          >
-            <Avatar
-              size="lg"
-              source={{
-                uri: "https://pbs.twimg.com/profile_images/1352844693151731713/HKO7cnlW_400x400.jpg",
-              }}
-            ></Avatar>
-            <VStack>
-              <Text style={{ fontWeight: "bold" }} color="#000000">
-                {event.createur.prenom} {event.createur.nom}
-              </Text>
-              <Text>@ </Text>
-              <Text>{event.quartier.name}</Text>
-            </VStack>
-            {dateEvent}
-          </HStack>
-          <Text>{event.content}</Text>
-          <Box alignItems="center">
-            <Image
-              source={{
-                uri: event.image
-                  ? event.image
-                  : "https://www.wallpapersun.com/wp-content/uploads/2021/05/Hasbulla-Wallpaper-13.jpg",
-              }}
-              alt="Alternate Text"
-              size={"xl"}
-              marginTop={5}
-            />
-          </Box>
-          <HStack
-            justifyContent="space-between"
-            alignItems="flex-end"
-            name="likecommentshare"
-            style={{ adding: 10, marginTop: 40 }}
-          >
-            <Text>
-              <AntDesign
-                name="like2"
-                size={24}
-                color="#B6B6B6"
-                onPress={() => handleLike()}
-              />
-              {countLikePost}
-            </Text>
-            <VStack>
-              <Button
-                title="Commentaires"
-                bg="#62ADEB"
-                my={2}
-                onPress={() => handleComment(event._id)}
-              >
-                Commentaires
-              </Button>
-              <Button
-                title="Aller à l'évènement"
-                bg="#62ADEB"
-                onPress={() =>
-                  props.navigation.navigate("Event", { idEvent: event._id })
-                }
-              >
-                Allez à l'évènement
-              </Button>
-            </VStack>
-            <Modal isOpen={showModal} onClose={() => closeComment()}>
-              <Modal.Content width="100%">
-                <Modal.CloseButton />
-                <Modal.Header alignItems="center">Commentaires</Modal.Header>
-                <Modal.Body>
-                  <ScrollView>{comments}</ScrollView>
-                </Modal.Body>
-                <Modal.Footer>{commentInput}</Modal.Footer>
-              </Modal.Content>
-            </Modal>
-            <FontAwesome5 name="share-square" size={24} color="#B6B6B6" />
-          </HStack>
-        </Box>
-      </VStack>
+      <Card
+        key={event._id}
+        item={event}
+        handleComment={handleComment}
+        isEvent
+      />
     );
   });
-  let postList = feedList.map((post, index) => {
-    const date = post.date;
-    const newDate = new Date(date);
-    let dateHours = newDate.getHours();
-    if (dateHours < 10) {
-      dateHours = `0${dateHours}`;
-    }
-    let dateMinutes = newDate.getMinutes();
-    if (dateMinutes < 10) {
-      dateMinutes = `0${dateMinutes}`;
-    }
-    const dateWeek = newDate.toLocaleDateString();
-    const datePost = (
-      <VStack alignItems="center">
-        <Text color="#B6B6B6" style={{ fontSize: 10 }}>
-          {dateWeek} - {dateHours}:{dateMinutes}
-        </Text>
-      </VStack>
-    );
-    return (
-      <VStack key={index}>
-        <Box
-          bg="#FFFFFF"
-          p={4}
-          style={{
-            marginTop: 10,
-            alignSelf: "center",
-            width: 410,
-          }}
-        >
-          <HStack
-            style={{
-              space: 3,
-              marginBottom: 3,
-              justifyContent: "space-between",
-              alignItems: "center",
-              borderBottomColor: "#B6B6B6",
-              borderBottomWidth: 1,
-            }}
-          >
-            <HStack
-              style={{
-                space: 3,
-                marginBottom: 3,
-                alignItems: "center",
-              }}
-            >
-              <Avatar
-                size="lg"
-                source={{
-                  uri: "https://pbs.twimg.com/profile_images/1352844693151731713/HKO7cnlW_400x400.jpg",
-                }}
-              ></Avatar>
-              <VStack>
-                <Text
-                  style={{ fontWeight: "bold", marginLeft: 5, fontSize: 18 }}
-                  color="#000000"
-                >
-                  {post.createur.prenom} {post.createur.nom}
-                </Text>
-                <Text style={{ marginLeft: 5 }}>{post.quartier.name}</Text>
-                {datePost}
-              </VStack>
-            </HStack>
-          </HStack>
-          <Text>{post.content}</Text>
-          <Box alignItems="center">
-            <Image
-              source={{
-                uri: post.image
-                  ? post.image
-                  : "https://www.wallpapersun.com/wp-content/uploads/2021/05/Hasbulla-Wallpaper-13.jpg",
-              }}
-              alt="Alternate Text"
-              size={"xl"}
-              marginTop={5}
-              marginBottom={5}
-            />
-          </Box>
-          <HStack
-            justifyContent="space-between"
-            alignItems="center"
-            name="likecommentshare"
-            style={{ padding: 10, marginTop: 0 }}
-          >
-            <Text>
-              <AntDesign
-                name="like2"
-                size={24}
-                color="#B6B6B6"
-                onPress={() => handleLike()}
-              />
-              {countLikePost}
-            </Text>
-            <MaterialCommunityIcons
-              name="comment-multiple-outline"
-              size={24}
-              color="#62ADEB"
-              onPress={() => handleComment(post._id)}
-            />
-            <Ionicons name="share-outline" size={28} color="#B6B6B6" />
-          </HStack>
-        </Box>
-        <Modal isOpen={showModal} onClose={() => closeComment()}>
-          <Modal.Content width="100%">
-            <Modal.CloseButton />
-            <Modal.Header alignItems="center">Commentaires</Modal.Header>
-            <Modal.Body>
-              <ScrollView>{comments}</ScrollView>
-            </Modal.Body>
-            <Modal.Footer>{commentInput}</Modal.Footer>
-          </Modal.Content>
-        </Modal>
-      </VStack>
-    );
+  let postList = feedList.map((post) => {
+    return <Card key={post._id} item={post} handleComment={handleComment} />;
   });
 
   return (
@@ -457,6 +214,16 @@ function FeedScreen(props) {
         {postList}
         {events}
       </ScrollView>
+      <Modal isOpen={showModal} onClose={() => closeComment()}>
+        <Modal.Content width="100%">
+          <Modal.CloseButton />
+          <Modal.Header alignItems="center">Commentaires</Modal.Header>
+          <Modal.Body>
+            <ScrollView>{comments}</ScrollView>
+          </Modal.Body>
+          <Modal.Footer>{commentInput}</Modal.Footer>
+        </Modal.Content>
+      </Modal>
     </View>
   );
 }
