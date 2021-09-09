a14167cd4eae94b8096f2c895ee0bce6c49b2ea5
Lucas Pieroni <pieroni.lucas@gmail.com>
2021-09-09 12:23:28 +0200

    card profilescreen event + post

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -17,20 +17,11 @@ function FeedScreen(props) {
 
   const [eventList, setEventList] = useState([]);
   // console.log("eventList:", eventList);
-
   const [commentList, setCommentList] = useState([]);
   // console.log("commentList", commentList);
 
-  const [commentValue, setCommentValue] = useState("");
-  console.log("commentaire récupéré:", commentValue);
-
-  const [postId, setPostId] = useState("");
-
   console.log("postId:", postId);
   // console.log("feedlist:", feedList);
-
-  const [showModal, setShowModal] = useState(false);
-  console.log("HOOOOOOST", HOST);
 
   const handleNavigEvent = function () {
     console.log("click");
@@ -44,17 +35,6 @@ function FeedScreen(props) {
   const handleFeed = () => {
     props.navigation.navigate("feed");
   };
-  const handleComment = (idPost) => {
-    console.log("click comment");
-    setShowModal(true);
-    setPostId(idPost);
-  };
-
-  const closeComment = () => {
-    setShowModal(false);
-  };
-
-  //ENVOI COMMENTAIRE AU BACK VIA ROUTE /comment
   const sendComment = async () => {
     console.log(
       "commentaire envoyé à /comment",
@@ -70,12 +50,25 @@ function FeedScreen(props) {
     });
     setCommentValue("");
   };
+  // const handleComment = (idPost) => {
+  //   console.log("click comment");
+  //   setShowModal(true);
+  //   setPostId(idPost);
+  // };
+
+  // const closeComment = () => {
+  //   setShowModal(false);
+  // };
+
+  //ENVOI COMMENTAIRE AU BACK VIA ROUTE /comment
+
   function sortByDate(arr) {
     arr.sort(function (a, b) {
       return Number(new Date(b.date)) - Number(new Date(a.date));
     });
     return arr;
   }
+  const [postId, setPostId] = useState("");
 
   //APPEL /feed POUR AFFICHER LES POSTS DANS LE FEED
   useEffect(() => {
@@ -96,98 +89,20 @@ function FeedScreen(props) {
     requestFeed();
   }, []);
 
-  let commentInput = (
-    <HStack
-      style={{ width: "100%", justifyContent: "center", alignItems: "center" }}
-    >
-      <Input
-        w="80%"
-        mx={3}
-        placeholder="Ecrire un commentaire ..."
-        _light={{
-          placeholderTextColor: "blueGray.400",
-        }}
-        _dark={{
-          placeholderTextColor: "blueGray.50",
-        }}
-        value={commentValue}
-        onChangeText={(value) => setCommentValue(value)}
-      />
-      <Ionicons
-        name="send"
-        size={24}
-        color="#62ADEB"
-        onPress={() => sendComment()}
-      />
-    </HStack>
-  );
-
-  let comments = commentList.map((comment, index) => {
-    if (postId == comment.post._id) {
-      return (
-        <HStack
-          key={index}
-          style={{
-            space: 3,
-            alignItems: "center",
-            marginBottom: 2,
-          }}
-        >
-          <Avatar
-            mr={2}
-            size="md"
-            source={{
-              uri: event.createur.profilePicture
-                ? event.createur.profilePicture
-                : "https://www.e-xpertsolutions.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png",
-            }}
-          ></Avatar>
-          <Stack>
-            <Text style={{ flexShrink: 1 }} color="#000000" bold={true}>
-              {comment.createur.nom} {comment.createur.prenom}
-            </Text>
-            <Text style={{ flexShrink: 1 }} my={2} color="#000000">
-              {comment.content}
-            </Text>
-          </Stack>
-        </HStack>
-      );
-    }
-  });
   let events = eventList.map((event, index) => {
-    return (
-      <Card
-        key={event._id}
-        item={event}
-        handleComment={handleComment}
-        isEvent
-      />
-    );
+    return <Card key={event._id} item={event} isEvent />;
   });
   let postList = feedList.map((post) => {
-    return <Card key={post._id} item={post} handleComment={handleComment} />;
+    return <Card key={post._id} item={post} />;
   });
 
   const tableauEventsAndPosts = sortByDate(feedList.concat(eventList));
 
   let feedListSorted = tableauEventsAndPosts.map((postOrEvent) => {
     if (postOrEvent.dateFin === undefined) {
-      return (
-        <Card
-          key={postOrEvent._id}
-          item={postOrEvent}
-          handleComment={handleComment}
-        />
-      );
+      return <Card key={postOrEvent._id} item={postOrEvent} />;
     } else {
-      return (
-        <Card
-          key={postOrEvent._id}
-          item={postOrEvent}
-          handleComment={handleComment}
-          isEvent
-        />
-      );
+      return <Card key={postOrEvent._id} item={postOrEvent} isEvent />;
     }
   });
 
@@ -242,16 +157,6 @@ function FeedScreen(props) {
         />
       </HStack>
       <ScrollView style={{ marginTop: 10 }}>{feedListSorted}</ScrollView>
-      <Modal isOpen={showModal} onClose={() => closeComment()}>
-        <Modal.Content width="100%">
-          <Modal.CloseButton />
-          <Modal.Header alignItems="center">Commentaires</Modal.Header>
-          <Modal.Body>
-            <ScrollView>{comments}</ScrollView>
-          </Modal.Body>
-          <Modal.Footer>{commentInput}</Modal.Footer>
-        </Modal.Content>
-      </Modal>
     </View>
   );
 }
--- screens/ProfilScreen.js
+++ screens/ProfilScreen.js
@@ -1,23 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, ScrollView } from "react-native";
+
+import Card from "../components/Card";
 import {
   Box,
-  Center,
   Avatar,
-  NativeBaseProvider,
-  Badge,
-  Flex,
-  Spacer,
   HStack,
   VStack,
   Image,
-  Stack,
   Modal,
-  Input,
   Button,
   AspectRatio,
-  Heading,
-  Icon,
 } from "native-base";
 import {
   Entypo,
@@ -34,10 +27,11 @@ import { useIsFocused } from "@react-navigation/native";
 
 const ProfilScreen = (props) => {
   const [userPost, setUserPost] = useState([]);
-  console.log("State userPost", userPost);
+  // console.log("State userPost", userPost);
+  const [userEvent, setUserEvent] = useState([]);
+  console.log("State userEvent", userEvent);
   const [user, setUser] = useState({});
-  console.log("user", user);
-  const [countLikePost, setCountLikePost] = useState(0);
+  // console.log("user", user);
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
@@ -48,9 +42,11 @@ const ProfilScreen = (props) => {
         body: `token=${props.token}`,
       });
       const userPost = await rawUserPost.json();
-      // console.log('userPost',userPost)
+      // console.log("userPost", userPost);
       setUserPost(userPost.userPosts);
+      // console.log("userPost.userPost", userPost.userPosts);
       setUser(userPost.user);
+      setUserEvent(userPost.userEvents);
     };
     fetchUserPost();
   }, [useIsFocused()]);
@@ -58,92 +54,54 @@ const ProfilScreen = (props) => {
   const handleGoEdit = () => {
     props.navigation.navigate("edit");
   };
+  const handleComment = (idPost) => {
+    console.log("click comment");
+    setShowModal(true);
+    setPostId(idPost);
+  };
 
-  let postList = userPost.map((post, index) => {
-    // console.log("postList", postList);
-    return (
-      <VStack key={index}>
-        <Box
-          bg="#FFFFFF"
-          p={4}
-          style={{
-            marginTop: 10,
-            alignSelf: "center",
-            width: 350,
-            borderBottomRightRadius: 20,
-            borderBottomLeftRadius: 20,
-            borderTopRightRadius: 20,
-            borderTopLeftRadius: 20,
-          }}
-        >
-          <HStack
-            style={{
-              space: 3,
-              alignItems: "center",
-              marginBottom: 1,
-            }}
-          >
-            <Avatar
-              size="md"
-              source={{
-                uri: user.profilePicture,
-              }}
-            ></Avatar>
-            <Text style={{ flexShrink: 1 }} color="#000000">
-              {user.prenom} {user.nom}
-              <Entypo name="shop" size={24} color="black" /> @ nom d'enseigne +
-              {post.quartier.name}
-            </Text>
-          </HStack>
-          <Text>{post.content}</Text>
-          <Box alignItems="center">
-            <Image
-              source={{
-                uri: post.image
-                  ? post.image
-                  : "https://www.wallpapersun.com/wp-content/uploads/2021/05/Hasbulla-Wallpaper-13.jpg",
-              }}
-              alt="Alternate Text"
-              size={"xl"}
-              marginTop={5}
-            />
-          </Box>
-          <HStack
-            justifyContent="space-between"
-            name="likecommentshare"
-            style={{ flex: 0, padding: 10, marginTop: 40 }}
-          >
-            <Text>
-              <AntDesign
-                name="like2"
-                size={24}
-                color="#B6B6B6"
-                onPress={() => handleLike()}
-              />
-              {countLikePost}
-            </Text>
-            <Button
-              title="Commentaires"
-              color="#62ADEB"
-              onPress={() => handleComment()}
-            >
-              Commentaires
-            </Button>
-            <Modal isOpen={showModal} onClose={() => closeComment()}>
-              <Modal.Content width="100%">
-                <Modal.CloseButton />
-                <Modal.Header alignItems="center">Commentaires</Modal.Header>
-                <Modal.Body>test</Modal.Body>
-                <Modal.Footer></Modal.Footer>
-              </Modal.Content>
-            </Modal>
-            <FontAwesome5 name="share-square" size={24} color="#B6B6B6" />
-          </HStack>
-        </Box>
-      </VStack>
-    );
+  let postList = userPost.map((post) => {
+    return <Card key={post.id} item={post} handleComment={handleComment} />;
   });
 
+  let events = userEvent.map((event) => {
+    return (
+      <Card
+        key={event._id}
+        item={event}
+        handleComment={handleComment}
+        isEvent
+      />
+    );
+  });
+  function sortByDate(arr) {
+    arr.sort(function (a, b) {
+      return Number(new Date(b.date)) - Number(new Date(a.date));
+    });
+    return arr;
+  }
+  const tableauEventsAndPosts = sortByDate(userPost.concat(userEvent));
+
+  let feedListSorted = tableauEventsAndPosts.map((postOrEvent) => {
+    if (postOrEvent.dateFin === undefined) {
+      return (
+        <Card
+          key={postOrEvent._id}
+          item={postOrEvent}
+          handleComment={handleComment}
+        />
+      );
+    } else {
+      return (
+        <Card
+          key={postOrEvent._id}
+          item={postOrEvent}
+          handleComment={handleComment}
+          isEvent
+        />
+      );
+    }
+  });
   return (
     <ScrollView>
       <Box>
@@ -164,7 +122,7 @@ const ProfilScreen = (props) => {
           style={{ alignItems: "center" }}
         >
           <Avatar
-            zIndex={2}
+            Index={2}
             size="2xl"
             source={{
               uri: user.profilePicture
@@ -201,7 +159,6 @@ const ProfilScreen = (props) => {
             Media
           </Button>
         </HStack>
-
         <HStack
           style={{ justifyContent: "center", marginBottom: 10, marginTop: 60 }}
           marginTop="3"
@@ -219,7 +176,7 @@ const ProfilScreen = (props) => {
             Edit
           </Button>
         </HStack>
-        {postList}
+        {feedListSorted}
       </Box>
     </ScrollView>
   );
